16b0836d9677ce15501d4dc4759d3e9323b46453
shonya3 <poeshonya3@gmail.com>
2026-05-27 21:59:44 +0300

    add vite-plugin-typed-css-modules

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
+import typedCssModulesPlugin from "vite-plugin-typed-css-modules";
 
 export default defineConfig({
   server: { port: 3000 },
@@ -10,5 +11,6 @@ export default defineConfig({
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     viteReact(),
+    typedCssModulesPlugin()
   ],
 });
