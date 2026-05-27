1d03f6ea314cbc5915fc28546f556ad40e3c200a
shonya3 <poeshonya3@gmail.com>
2026-05-27 14:26:13 +0300

    add viewport meta

--- src/reader/layout.tsx
+++ src/reader/layout.tsx
@@ -48,6 +48,7 @@ function Shell({
     <html>
       <head>
         <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{title}</title>
         <meta name="description" content={description || "Path of Exile patch notes reader"} />
         <meta property="og:title" content={title} />
